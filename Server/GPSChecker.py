38c535601f9b7e2aff16660aad522a9d4aeac76e
Andy Re <anandy@web.de>
2018-04-09 12:17:37 +0200

    Implemented Haversine formula.
    Compares two gps coords in dd format and return the distance.

--- Server/GPSChecker.py
+++ Server/GPSChecker.py
@@ -1,9 +1,9 @@
-import struct
+import math
 
 class GpsComparer:
 
     # Message id's of the multiwii serial protocol
-    EARTH_RADIUS = 100
+    __EARTH_RADIUS = 6371000
 
 
 
@@ -12,9 +12,16 @@ class GpsComparer:
 
     """
     Expects two GPS coords in dd format.
-    Returns the distance between the coords in meters.
+    Uses the Haversine formular to calculate the distance.
+    Returns the distance in meters.
     """
     def getDistance(self, coord1, coord2):
+        p1 = (coord1.getLon() - coord2.getLon()) * math.cos(0.5 * (coord1.getLat() + coord2.getLat()))
+        p2 = (coord1.getLat() - coord2.getLat())
+        """
+        hypot(x,y) = sqrt(x * x + y * y)
+        """
+        return self.__EARTH_RADIUS * math.hypot(p1, p2)
 
 
 
